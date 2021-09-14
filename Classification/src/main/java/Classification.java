e5b0fd18e4f426eaf8d83570507eb57a045281bd
omid haghighatgoo <omid.haghighatgoo@gmail.com>
2021-09-14 20:31:43 +0430

    Add files via upload

--- Classification/src/main/java/Classification.java
+++ Classification/src/main/java/Classification.java
@@ -1,46 +1,51 @@
 import weka.attributeSelection.AttributeSelection;
 import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
+import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
 
+import javax.swing.*;
 
 public class Classification {
 
 
-
-
     public void toClassify() throws Exception {
+
+
         DataSource source = new DataSource("zoo.arff");
-        Instances data = source.getDataSet();
-        System.out.println(data.numInstances() + " instances loaded.");
-        System.out.println(data.toString());
-
-
-        System.out.println("=============================================================");
+        Instances data= source.getDataSet();
 
         Remove remove = new Remove();
-        remove.setOptions(new String[]{"-R" , "1"});
+        remove.setOptions(new String[] {"-R" , "1"});
         remove.setInputFormat(data);
-        Instances preparedData = Filter.useFilter(data,remove);
-        System.out.println(preparedData.toString());
-
-
-        System.out.println("=============================================================");
-
+        Instances preparedData = Filter.useFilter(data , remove);
 
         AttributeSelection attributeSelection = new AttributeSelection();
-        attributeSelection.setEvaluator( new InfoGainAttributeEval());
-        attributeSelection.setSearch( new Ranker());
+        attributeSelection.setEvaluator(new InfoGainAttributeEval());
+        attributeSelection.setSearch(new Ranker());
         attributeSelection.SelectAttributes(preparedData);
 
-        int[] selectedAttributes = attributeSelection.selectedAttributes();
+        int[] selectedAttribute = attributeSelection.selectedAttributes();
+        System.out.println(Utils.arrayToString(selectedAttribute));
 
-        System.out.println(selectedAttributes.length);
-        System.out.println(Utils.arrayToString(selectedAttributes));
+        J48 tree = new J48();
+        tree.setOptions(new String[]{"-U"});
+        tree.buildClassifier(preparedData);
+        System.out.println(tree.toString());
+
+        TreeVisualizer treeVisualizer = new TreeVisualizer(null , tree.graph(), new PlaceNode2());
+        JFrame jFrame = new JFrame("Virtual Learning Youtube Channel");
+        jFrame.setSize(1920,1080);
+        jFrame.getContentPane().add(treeVisualizer);
+        jFrame.setVisible(true);
+        treeVisualizer.fitToScreen();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
